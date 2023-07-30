d8fb0f6adc182203b88e6af5a1feeeabab9c3307
masudrana5 <abdullainfo30@gmail.com>
2023-07-30 22:35:10 +0600

    initial setup typescript leanring process

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+let age: number = 23;
+
+type IUser = {
+    name: string;
+    role: string;
+    number: string;
+    email: string;
+    age: number;
+}
+
+const person: IUser ={
+    name: "masud",
+    role: "student",
+    number: "215465456",
+    email: "rana@gmail.com",
+    age: 24
+}
+
+console.log(person);
